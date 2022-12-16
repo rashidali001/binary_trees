94ed67af2a51e3e39fa7d2b7379b86e1f53fd4e9
rashidali001 <rashidestonian@gmail.com>
2022-12-17 01:26:49 +0300

    Binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,8 +8,9 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+    binary_tree_t *newnode;
 
+    newnode= malloc(sizeof(binary_tree_t));
     if (newnode == NULL)
         return (NULL);
 
