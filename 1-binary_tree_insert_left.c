6c8bcb5c026e3cb19dfae5afdf9e89b2546dfe4a
rashidali001 <rashidestonian@gmail.com>
2022-12-17 01:01:51 +0300

    Binary trees

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left  - inserts to the left
+ *
+ * @parent: pointer to the node to insert left
+ * @value: value to store in the new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    if (parent == NULL)
+        return NULL;
+
+    if (parent->left == NULL)
+    {
+        binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+        newnode->n = value;
+        newnode->parent = parent;
+        newnode->left = NULL;
+        newnode->right = NULL;
+        parent->left = newnode;
+        return newnode;
+    }
+    else{
+        binary_tree_t *oldnode;
+        oldnode = parent->left;
+        binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+        newnode->n = value;
+        newnode->parent = oldnode->parent;
+        newnode->left = oldnode;
+        newnode->right = NULL;
+        oldnode->parent = newnode;
+        parent->left = newnode;
+        return newnode;
+    }
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -46,5 +46,9 @@ void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/* 1.Insert left Task */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 #endif
 
