3a306bcbba11e2b183afd5e9fd3cf3d430696dd8
rashidali001 <rashidestonian@gmail.com>
2022-12-18 01:18:55 +0300

    Binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -54,5 +54,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
+/* Is leaf */
+
+int binary_tree_is_leaf(const binary_tree_t *node);
+
 #endif
 
